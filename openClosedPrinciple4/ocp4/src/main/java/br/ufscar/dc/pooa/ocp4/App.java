c8e53b3c0bf9e3a51585af01586606e38984d29d
Daniel Lucrédio <dlucredio@gmail.com>
2020-11-21 18:39:24 -0300

    Exemplo para o trabalho sobre OCP

---
+++ openClosedPrinciple4/ocp4/src/main/java/br/ufscar/dc/pooa/ocp4/App.java
@@ -0,0 +1,48 @@
+package br.ufscar.dc.pooa.ocp4;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        String strNow = DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm-ss").format(LocalDateTime.now());
+        try (PrintWriter pw = new PrintWriter(new FileWriter(new File(String.format("dump-%s.csv", strNow))))) {
+            pw.println("Tipo;Notícia;Link");
+            Document doc = Jsoup.connect("https://www.globo.com").get();
+            Elements titles = doc.select("p.hui-premium__title");
+            for (Element t : titles) {
+                pw.print("Principal;\"" + t.text() + "\";");
+                Element parent = t.parent();
+                while (parent != null && !parent.tagName().equals("a")) {
+                    parent = parent.parent();
+                }
+                if (parent != null && parent.tagName().equals("a")) {
+                    pw.print("\""+parent.attr("href")+"\"");
+                }
+                pw.print("\n");
+            }
+            
+            Elements titles2 = doc.select("p.hui-highlight-title");
+            for (Element t : titles2) {
+                pw.print("Secundário;\"" + t.text() + "\";");
+                Element parent = t.parent();
+                while (parent != null && !parent.tagName().equals("a")) {
+                    parent = parent.parent();
+                }
+                if (parent != null && parent.tagName().equals("a")) {
+                    pw.print("\""+parent.attr("href")+"\"");
+                }
+                pw.print("\n");
+            }            
+        }
+    }
+}
